43ef894b65fa433d83610083ac8380eecc2ab6f5
nick <nick.bouldien@gmail.com>
2018-11-09 22:55:18 -0600

    add sampleData for testing/development and now have basic routes for countries

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Hapi = require('hapi');
+const Boom = require('boom');
+const sampleData = require('./sampleData');
 
 const isDev = process.env.NODE_ENV !== "production";
 
@@ -14,7 +16,37 @@ server.route({
   method: 'GET',
   path: '/',
   handler: function (request, h) {
-    return 'welcome';
+    return 'hello world!';
+  }
+});
+
+server.route({
+  method: ['GET', 'POST'],
+  path: '/api/test',
+  handler: function (request, h) {
+    const successObj = { 'success!': true };
+    return JSON.stringify(successObj);
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/countries/{id}',
+  handler: function (request, h) {
+    const country = sampleData.find(country => country.id === Number(request.params.id) );
+    // const sortKey = request.query.sortKey;
+    if (!country) {
+      return Boom.notFound(`Country with id ${request.params.id} not found!`);
+    }
+    return country;
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/countries',
+  handler: function (request, h) {
+    return sampleData;
   }
 });
 
---
+++ sampleData.js
@@ -0,0 +1,46 @@
+// sample data (paises)
+
+/*
+  units:
+  area: km^2
+  GDP: USD
+*/
+
+module.exports = [
+  {
+    id: 1,
+    area: 9629091,
+    name: 'United States of America',
+    currency: "Dollar",
+    population: 310232863,
+    GDP: 143000000000,
+    language: 'English',
+  },
+  {
+    id: 2,
+    area: 8511965,
+    name: 'Brasil',
+    population: 201100000,
+    currency: "Real",
+    GDP: 2000000000,
+    language: 'Portuguese',
+  },
+  {
+    id: 3,
+    area: 1972550,
+    name: 'Mexico',
+    population: 112468855,
+    currency: "Peso",
+    GDP: 1600000000,
+    language: 'Spanish',
+  },
+  {
+    id: 4,
+    area: 1138910,
+    name: 'Colombia',
+    population: 44205293,
+    currency: "Peso",
+    GDP: 39400000000,
+    language: 'Spanish',
+  },
+];
